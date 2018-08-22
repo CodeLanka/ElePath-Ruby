0c8ecadb324557a2368c5105bd0ebe8fe2cdca0b
Isuru Abeywardana <vpisabeywardana@gmail.com>
2018-08-22 17:23:27 +0800

    add post endpoint for locations (#2)
    * add a post request end point for location
    
    * removed unwated data
    
    * get all locations of specific elephant
    
    * Add post request for location
    
    * add post request end point for location

--- app/api/path_finder/v1/elephants.rb
+++ app/api/path_finder/v1/elephants.rb
@@ -19,14 +19,34 @@ module V1
         end
 
         resource :location do
+          desc 'Create a Location.'
+          params do
+            requires :location, type: Hash do
+               requires :date, type: DateTime, desc: 'Location date'
+               requires :latitude, type: Float, desc: 'latitude of the location.'
+               requires :longitude, type: Float, desc: 'longitude of the location'
+               requires :user_id, type: Integer, desc: 'User ID'
+            end
+          end
+          post do
+
+             @elephant = Elephant.find(params[:id])
+             @location = Location.new(params[:location])
+             @location = @elephant.location.create!(params[:location])
+             #@elephant.update(stock: @location.newStock)
+
+             #location = Location.create!({date: params[:date], latitude: params[:latitude], longitude: params[:longitude], elephant_id: params[:id],  user_id: params[:user_id]})
+             present :status, 201
+             present :location, @location
+          end
 
 
           desc 'Get all locations of specific Elephant'
           params do
           end
           get do
+              location = Location.where('elephant_id = :id ', id: params[:id])
 
-              location = Location.where('elephant_id LIKE :id ', id: params[:id])
               present location, with: Entities::Location
           end
         end
