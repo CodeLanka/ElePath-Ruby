23cb0bf893e383e4c6f0c37fcc480595e2dd058e
Isuru Abeywardana <vpisabeywardana@gmail.com>
2018-08-23 14:41:20 +0800

    Add endpoints for get all users and update user details (#3)
    * add a post request end point for location
    
    * removed unwated data
    
    * get all locations of specific elephant
    
    * Add post request for location
    
    * add post request end point for location
    
    * add user get add and update endpoints

--- app/api/base.rb
+++ app/api/base.rb
@@ -2,4 +2,5 @@
 class Base< Grape::API
   	mount V1::Elephants
   	mount V1::Locations
+  	mount V1::Users
 end
--- app/api/path_finder/v1/elephants.rb
+++ app/api/path_finder/v1/elephants.rb
@@ -30,14 +30,11 @@ module V1
           end
           post do
 
-             @elephant = Elephant.find(params[:id])
-             @location = Location.new(params[:location])
-             @location = @elephant.location.create!(params[:location])
-             #@elephant.update(stock: @location.newStock)
-
-             #location = Location.create!({date: params[:date], latitude: params[:latitude], longitude: params[:longitude], elephant_id: params[:id],  user_id: params[:user_id]})
+             elephant = Elephant.find(params[:id])
+             location = Location.new(params[:location])
+             location = elephant.location.create!(params[:location])
              present :status, 201
-             present :location, @location
+             present :location, location
           end
 
 
@@ -46,7 +43,6 @@ module V1
           end
           get do
               location = Location.where('elephant_id = :id ', id: params[:id])
-
               present location, with: Entities::Location
           end
         end
