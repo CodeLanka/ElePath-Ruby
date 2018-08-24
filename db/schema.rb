453f624e096ffdeb8b435c1ade90beefafa0c243
Isuru Abeywardana <vpisabeywardana@gmail.com>
2018-08-24 14:04:11 +0800

    post end point to add users (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,12 @@ ActiveRecord::Schema.define(version: 20180813133633) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "password"
     t.datetime "last_login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "locations", "elephants"
