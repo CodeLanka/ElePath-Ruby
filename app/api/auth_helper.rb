6810b637e02b3bdf0ab0a67faa20e1e022b2f523
isuruAb <vpisabeywardana@gmail.com>
2018-10-16 09:54:57 +0800

    Fixes: #31 move helpers to a new file

---
+++ app/api/auth_helper.rb
@@ -0,0 +1,29 @@
+module AuthHelper
+    def authenticate!
+        begin  # "try" block
+            if "Bearer"==/Bearer/.match(headers['Authorization'])[0]
+                token=headers['Authorization'].split(" ")[1]
+                decoded_token = JWT.decode token, ENV["SECRETKEY"], true, { algorithm: 'HS256' }
+            else
+                error!('Unauthorized.', 401)
+            end
+        
+        rescue # optionally: `rescue Exception => ex`
+            error!('Unauthorized.', 401)
+        end
+    end
+    def current_user
+        begin  # "try" block
+            if "Bearer"==/Bearer/.match(headers['Authorization'])[0]
+                token=headers['Authorization'].split(" ")[1]
+                decoded_token = JWT.decode token, ENV["SECRETKEY"], true, { algorithm: 'HS256' }
+                decoded_token[0]["user_id"]
+
+            else
+                error!('Unauthorized.', 401)
+            end
+        rescue # optionally: `rescue Exception => ex`
+            error!('Unauthorized.', 401)
+        end
+    end
+end
--- app/api/base.rb
+++ app/api/base.rb
@@ -2,37 +2,8 @@ require 'jwt'
 require 'grape-swagger'
 
 class Base< Grape::API
-	helpers do
-		def authenticate!
-			begin  # "try" block
-				if "Bearer"==/Bearer/.match(headers['Authorization'])[0]
-					token=headers['Authorization'].split(" ")[1]
-					decoded_token = JWT.decode token, ENV["SECRETKEY"], true, { algorithm: 'HS256' }
-				else
-					error!('Unauthorized.', 401)
-				end
-			
-			rescue # optionally: `rescue Exception => ex`
-				error!('Unauthorized.', 401)
-			end
-		end
-		def current_user
-			begin  # "try" block
-				if "Bearer"==/Bearer/.match(headers['Authorization'])[0]
-					token=headers['Authorization'].split(" ")[1]
-					decoded_token = JWT.decode token, ENV["SECRETKEY"], true, { algorithm: 'HS256' }
-					decoded_token[0]["user_id"]
-
-				else
-					error!('Unauthorized.', 401)
-				end
-			rescue # optionally: `rescue Exception => ex`
-				error!('Unauthorized.', 401)
-			end
-		end
-	end
 	format :json
-
+	helpers AuthHelper
   	mount V1::Elephants
   	mount V1::Locations
 	mount V1::Users
